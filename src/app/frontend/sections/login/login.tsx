59781c054985be1847de7cf054065407a47499f7
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2024-04-24 15:55:13 +0200

    feat: login, signup, auth config

---
+++ src/app/frontend/sections/login/login.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { login, signup } from './action';
+
+export default function LoginPage() {
+  const [formData, setFormData] = useState(new FormData());
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    formData.set(name, value);
+    setFormData(formData);
+  };
+
+  const handleLogin = () => {
+    login(formData);
+  };
+
+  const handleSignup = () => {
+    signup(formData);
+  };
+
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input 
+        id="email" 
+        name="email" 
+        type="email" 
+        required 
+        onChange={handleInputChange} 
+      />
+      <label htmlFor="password">Password:</label>
+      <input 
+        id="password" 
+        name="password" 
+        type="password" 
+        required 
+        onChange={handleInputChange} 
+      />
+      <button type="button" onClick={handleLogin}>Log in</button>
+      <button type="button" onClick={handleSignup}>Sign up</button>
+    </form>
+  );
+}
