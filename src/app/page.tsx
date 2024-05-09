e8c562fba46fd38932245bd7c2fad3e96fcd15cd
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2024-05-09 16:13:00 +0200

    feat: login, logout, signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import { logout } from "../frontend/sections/logout/actions"
 
 export default function Home() {
   return (
     <main className="bg-green-400 lex min-h-screen flex-col items-center justify-between p-24">
       <div>Development in new repo initiated</div>
+      <form action={logout}>
+        <button type="submit">
+        Logout
+        </button>
+      </form>
     </main>
   );
 }
