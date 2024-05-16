839beb12218e8f222dd9cda18f3e1342e49d8ef0
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-05-16 20:48:23 +0100

    chore: test .env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="bg-green-400 lex min-h-screen flex-col items-center justify-between p-24">
       <div>Development in new repo initiated</div>
+      <div>testing env variables</div>
       <form action={logout}>
         <button type="submit">
         Logout
